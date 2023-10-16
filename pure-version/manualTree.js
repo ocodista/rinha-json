96d0e86b1eda4b08504ec69e74bccb32a59d426d
Caio Borghi <caiohoborghi@gmail.com>
2023-10-16 14:14:27 -0300

    add initial version of parseObj, parsing string props

--- pure-version/manualTree.js
+++ pure-version/manualTree.js
@@ -1,56 +1,172 @@
-export const keyHTML = (key) => `<span class="json-key">${key}</span>`;
+/* eslint-disable no-case-declarations */
+export const valueType = {
+  key: "key",
+  string: "string",
+  notString: "notString",
+};
+
+export const htmlByType = {
+  [valueType.key]: (key) => `
+    <span class="json-key">${key}</span>
+  `,
+  [valueType.string]: (value) => `<span class="json-string">"${value}"</span>`,
+  [valueType.notString]: (value) => `<span class="json-value">${value}</span>`,
+};
 
 const INVALID_JSON = "JSON is invalid.";
-
+const STRING_INDICATOR = '"';
+const KEY_END_INDICATOR = ":";
 const ARRAY_START_INDICATOR = "[";
-const ARRAY_END_INDICATOR = "]";
-const STARTING_TOKEN = /[{[]/;
-const OBJECT_END = "}";
+const PROP_SEPARATOR = ",";
+const STARTING_JSON_REGEX = /[{[]/;
+const STARTING_VALUE_REGEX = /("|t|f|[0-9]|n|{)/;
+const OBJ_PROP_OR_END_REGEX = /[},]/;
 
 export class RinhaParser {
   position = 0;
-  char;
   buffer = "";
   json = "";
+  htmlTags = [];
 
   construct() {}
 
+  char() {
+    return this.json[this.position];
+  }
+
   readNextChar() {
     this.position++;
-    this.char = this.json[this.position];
+    if (this.position > this.json.length) throw new Error(INVALID_JSON);
+    return this.char();
+  }
+
+  readUntil(char) {
+    let readBuffer = this.char();
+    let stop = false;
+    do {
+      readBuffer += this.readNextChar();
+      const hasReachedChar = readBuffer.at(-1) === char;
+      if (hasReachedChar) {
+        // should stop
+        const isEscaped = readBuffer.at(-2) === "\\";
+        if (!isEscaped) stop = true;
+      }
+    } while (!stop);
+    readBuffer = readBuffer.slice(0, -1);
+    return readBuffer;
+  }
+
+  readKey() {
+    this.expect(STRING_INDICATOR);
+    let key = this.char();
+    while (key.at(-1) !== STRING_INDICATOR) {
+      key += this.readNextChar();
+    }
+    key = key.slice(0, -1); // remove last " from key
+    this.expect(STRING_INDICATOR);
+    return key;
+  }
+
+  readValue() {
+    const valueStart = this.match(STARTING_VALUE_REGEX);
+    let value = "",
+      type = "string";
+    switch (valueStart) {
+      case "f":
+        this.expect("alse");
+        value = "false";
+        type = valueType.notString;
+        break;
+      case "t":
+        this.expect("rue");
+        value = "true";
+        type = valueType.notString;
+        break;
+      case "n":
+        this.expect("ull");
+        value = "null";
+        type = valueType.notString;
+        break;
+      case '"':
+        const strValue = this.readUntil(STRING_INDICATOR);
+        this.expect(STRING_INDICATOR);
+        value = strValue;
+        type = valueType.string;
+        break;
+      case "{":
+        // TODO: Parse nested object
+        break;
+      default:
+        if (!isNaN(this.char())) {
+          throw new Error(INVALID_JSON);
+        }
+        const number = this.readUntil(PROP_SEPARATOR);
+        if (!isNaN(number)) throw new Error(INVALID_JSON);
+        value = number;
+        type = valueType.notString;
+        break;
+    }
+    return { value, type };
+
+    // expect value end, that can be ,]}
+  }
+
+  ignoreSpace() {
+    while (this.char() === " " || this.char() === "\n") {
+      this.readNextChar();
+    }
   }
 
   expect(nextChars) {
+    this.ignoreSpace();
     for (let i = 0; i < nextChars.length; i++) {
-      const nextChar = nextChars[i];
-      if (this.char !== nextChar) {
-        throw new Error(INVALID_JSON);
+      const expected = nextChars[i];
+      if (this.char() !== expected) {
+        // TODO: uncomment this to debug
+        //console.log({ char: this.char(), expected: nextChar });
+        throw new Error(INVALID_JSON, { expected, char: this.char() });
       }
       this.readNextChar();
     }
   }
 
   match(expectedRegex) {
-    let char = this.char;
-    if (!expectedRegex.test(this.char)) {
+    let char = this.char();
+    if (!expectedRegex.test(this.char())) {
       throw new Error(INVALID_JSON);
     }
     this.readNextChar();
     return char;
   }
 
+  parseArray() {
+    //this.expect(ARRAY_END_INDICATOR);
+  }
+
+  parseObject() {
+    let hasNextProp = false;
+    do {
+      const key = this.readKey();
+      this.htmlTags.push(htmlByType[valueType.key](key));
+      this.expect(KEY_END_INDICATOR);
+      const { type, value } = this.readValue();
+      this.htmlTags.push(htmlByType[type](value));
+      const endOrNewProp = this.match(OBJ_PROP_OR_END_REGEX);
+      hasNextProp = endOrNewProp === PROP_SEPARATOR;
+    } while (hasNextProp);
+  }
+
   parse(str) {
     this.json = str;
     if (!str) throw new Error(INVALID_JSON);
-    this.char = this.json[0];
-    const startToken = this.match(STARTING_TOKEN);
+    this.position = 0;
+    const startToken = this.match(STARTING_JSON_REGEX);
+    this.ignoreSpace();
     if (startToken === ARRAY_START_INDICATOR) {
-      //handle array
-      console.log("is array", this.char, this.position);
-      this.expect(ARRAY_END_INDICATOR);
+      this.parseArray();
       return;
     }
-
-    this.expect(OBJECT_END);
+    this.parseObject();
+    return this.htmlTags;
   }
 }
--- pure-version/manualTree.spec.js
+++ pure-version/manualTree.spec.js
@@ -1,20 +1,76 @@
-import { test } from "node:test";
+import { test, describe } from "node:test";
 import assert from "node:assert";
-import { RinhaParser } from "./manualTree.js";
+import { RinhaParser, htmlByType, valueType } from "./manualTree.js";
 
-const parser = new RinhaParser();
-test("throws error for invalid JSON", () => {
-  assert.throws(() => parser.parse("}"));
-  assert.throws(() => parser.parse("]"));
-  assert.throws(() => parser.parse("a"));
-  assert.throws(() => parser.parse("n"));
-  assert.throws(() => parser.parse("1"));
+const parse = (str) => new RinhaParser().parse(str);
+
+describe("JSON Start", () => {
+  test("throws error for invalid JSON start", () => {
+    assert.throws(() => parse("}"));
+    assert.throws(() => parse("]"));
+    assert.throws(() => parse("a"));
+    assert.throws(() => parse("n"));
+    assert.throws(() => parse("1"));
+  });
+
+  test("does not throw for valid JSON array start", () => {
+    assert.doesNotThrow(() => parse("["));
+  });
 });
 
-test("does not throws for valid JSON", () => {
-  assert.doesNotThrow(() => parser.parse("{}"));
+describe("expect", () => {
+  const parser = new RinhaParser();
+  parser.json = '{ "name": "Caio Borghi" }';
+  parser.position = 0;
+
+  test("increases position if token is found next", () => {
+    parser.expect("{");
+    assert.equal(parser.position, 1);
+  });
+
+  test("works if pass only key, without spaces", () => {
+    parser.expect('"name"');
+    assert.equal(parser.position, 8);
+  });
+
+  test("throws error if next character is not found", () => {
+    assert.throws(() => {
+      parser.expect("not :");
+    });
+  });
 });
 
-test("does not throw with json array", () => {
-  assert.doesNotThrow(() => parser.parse("[]"));
+describe("read key", () => {
+  test.todo("throws if key is malformed");
+  test.todo("returns key if valid json");
+  test.todo(
+    "save rest of string to persistance if json ends before closing key",
+  );
 });
+
+describe("json string prop", () => {
+  const json = JSON.stringify({
+    name: "Caio",
+  });
+  test("does not throws", () => {
+    assert.doesNotThrow(() => {
+      parse(json);
+    });
+  });
+  const result = parse(json);
+  test("returns two html tags (key and value)", () => {
+    assert.equal(result.length, 2);
+  });
+  test("returns key html first", () => {
+    assert.equal(result[0], htmlByType[valueType.key]("name"));
+  });
+  test("returns string html second", () => {
+    assert.equal(result[1], htmlByType[valueType.string]("Caio"));
+  });
+});
+
+// describe("json numeric prop");
+// describe("json boolean prop");
+// describe("json array prop");
+// describe("json null prop");
+// describe("json object prop");
